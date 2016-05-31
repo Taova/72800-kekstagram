127353b90d186610817d01d4bc42e72136eab7d8
Taova Marina <taomarina@yandex.ru>
2016-05-31 14:35:48 +0300

    edit array in first dz

--- build/js/check.js
+++ build/js/check.js
@@ -8,19 +8,19 @@ function getMessage(a, b){
  if (typeof a === 'number'){
  	return "Переданное SVG-изображение содержит " + a + " объектов и " + b * 4+" атрибутов";
  }
- if (typeof a === 'object'){
- 	var sum = 0;
- 	for(var i = 0; i < a.length; i++){
- 		sum = sum + a[i];
- 		
- 	}
- 	return   "Количество красных точек во всех строчках изображения: " + sum;
- }
- if ( (Array.isArray(a)) && (Array.isArray(b)) ){
+
+if ( (Array.isArray(a)) && (Array.isArray(b)) ){
  	var square = 0;
  	for(var i = 0; i < a.length; i++){
  		square +=  a[i] * b[i];
  	}
  	return "Общая площадь артефактов сжатия: " + square + " пикселей";
- }
+
+ } else if (Array.isArray(a)){
+ 		var sum = 0;
+	 	for(var i = 0; i < a.length; i++){
+	 		sum = sum + a[i];	 		
+	 	}
+ 		return   "Количество красных точек во всех строчках изображения: " + sum;
+ 	}
 }
