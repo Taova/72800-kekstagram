5bb85062dd48d8f1e789910c161981a21a709970
Taova Marina <taomarina@yandex.ru>
2016-05-31 14:24:48 +0300

    edit first dz

--- build/js/check.js
+++ build/js/check.js
@@ -1,30 +1,26 @@
-function getMessage(a,b){
- if (typeof(a)==="boolean"){
+function getMessage(a, b){
+ if (typeof a ==='boolean'){
  	if (a){
  		return "Переданное GIF-изображение анимировано и содержит "+ b+" кадров";
- 	}else{
+ 	}
  	return "Переданное GIF-изображение не анимировано";
- 	} 	
  } 
- if (typeof(a)==="number"){
- 	return "Переданное SVG-изображение содержит "+a+" объектов и "+b * 4+" атрибутов";
+ if (typeof a === 'number'){
+ 	return "Переданное SVG-изображение содержит " + a + " объектов и " + b * 4+" атрибутов";
  }
- if (typeof(a)==="object"){
+ if (typeof a === 'object'){
  	var sum = 0;
- 	for(var i = 0; i<a.length; i++){
- 		sum = sum +a[i];
+ 	for(var i = 0; i < a.length; i++){
+ 		sum = sum + a[i];
  		
  	}
- 	return   "Количество красных точек во всех строчках изображения: "+sum;
+ 	return   "Количество красных точек во всех строчках изображения: " + sum;
  }
- if ((typeof(a)==="object")&&(typeof(b)==="object")){
+ if ( (Array.isArray(a)) && (Array.isArray(b)) ){
  	var square = 0;
- 	for(var i = 0; i<a.length; i++){
- 		for(var j = 0; j<b.length; j++){
- 		square = square +a[i]*b[i];
- 		
- 		}
+ 	for(var i = 0; i < a.length; i++){
+ 		square +=  a[i] * b[i];
  	}
- 	return "Общая площадь артефактов сжатия: "+square+" пикселей";
+ 	return "Общая площадь артефактов сжатия: " + square + " пикселей";
  }
 }
