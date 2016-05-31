ecfa0ce6078c242fca0769ace4717e3a323ce52c
Taova Marina <taomarina@yandex.ru>
2016-05-31 14:54:56 +0300

    edit style code

--- build/js/check.js
+++ build/js/check.js
@@ -1,26 +1,29 @@
 function getMessage(a, b){
  if (typeof a ==='boolean'){
- 	if (a){
- 		return "Переданное GIF-изображение анимировано и содержит "+ b+" кадров";
- 	}
- 	return "Переданное GIF-изображение не анимировано";
- } 
- if (typeof a === 'number'){
- 	return "Переданное SVG-изображение содержит " + a + " объектов и " + b * 4+" атрибутов";
+    if (a) {
+        return 'Переданное GIF-изображение анимировано и содержит ' + b +' кадров';
+    }
+    return 'Переданное GIF-изображение не анимировано';
  }
 
-if ( (Array.isArray(a)) && (Array.isArray(b)) ){
- 	var square = 0;
- 	for(var i = 0; i < a.length; i++){
- 		square +=  a[i] * b[i];
- 	}
- 	return "Общая площадь артефактов сжатия: " + square + " пикселей";
+ if (typeof a === 'number'){
+    return 'Переданное SVG-изображение содержит ' + a + ' объектов и ' + b * 4+' атрибутов';
+ }
 
- } else if (Array.isArray(a)){
- 		var sum = 0;
-	 	for(var i = 0; i < a.length; i++){
-	 		sum = sum + a[i];	 		
-	 	}
- 		return   "Количество красных точек во всех строчках изображения: " + sum;
- 	}
+if (Array.isArray(a) && Array.isArray(b)) {
+    var square = 0;
+
+    for(var i = 0; i < a.length; i++) {
+        square +=  a[i] * b[i];
+    }
+    return "Общая площадь артефактов сжатия: " + square + " пикселей";
+
+ } 
+ if (Array.isArray(a)) {
+    var sum = 0;
+    for(var i = 0; i < a.length; i++){
+      sum = sum + a[i];           
+      }
+    return   "Количество красных точек во всех строчках изображения: " + sum;
+  }
 }
