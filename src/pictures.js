6f2301fb87d6b9712f158e4d4723ac93e4e8512a
Taova Marina <taomarina@yandex.ru>
2016-06-14 16:55:27 +0300

    #12 Робкое знакомство с AJAX пока не все ошибки устранены еслинт

--- src/pictures.js
+++ src/pictures.js
@@ -10,6 +10,7 @@ formFilters.classList.add('hidden');
 
 var picturesContainer = document.querySelector('.pictures');
 var templateElement = document.querySelector('#picture-template');
+var divContainer = document.querySelector('#no-filters');
 var elementToClone;
 
 if ('content' in templateElement) {
@@ -36,9 +37,9 @@ function getPictureElement(data, container) {
     elemImgData.width = 182;
     elemImgData.height = 182;
   };
-  // elemImgData.onerror = function() {
-  //  element.classList.add('picture-load-failure');
-  // };
+  elemImgData.onerror = function() {
+    element.classList.add('picture-load-failure');
+  };
   elemImgData.src = data.url;
   return element;
 }
@@ -47,10 +48,9 @@ var getPictures = function(callback) {
   var xhr = new XMLHttpRequest();
 
   /** @param {ProgressEvent} */
-  xhr.onload = function (evt) {
+  xhr.onload = function(evt) {
     var loadedData = JSON.parse(evt.target.response);
     callback(loadedData);
-
   };
   // xhr.error = fuction(){
   //  picturesContainer.classList.add('picture-load-failure');
@@ -61,20 +61,27 @@ var getPictures = function(callback) {
 
 /** @param {Array.<Object>} pictures */
 var renderPictures = function(pictures) {
-  formFilters.innerHTML = '';
-  pictures.forEach( function(picture) {
+  picturesContainer.innerHTML = '';
+  pictures.forEach(function(picture) {
     getPictureElement(picture, picturesContainer);
   });
 };
 
 getPictures(function(loadedPictures) {
   pictures = loadedPictures;
+  setFiltrationImg();
   renderPictures(pictures);
 });
-// Список изображений изменяется  в зависимости переданных значаний filter
-/** @param {string} filter */
-var SetFiltrationImgId = function(filter) {
+// // Список изображений изменяется  в зависимости переданных значаний filter
+// /** @param {string} filter */
+var setFiltrationImgId = function(filter) {
   var filterImage = getFilterPictures(pictures, filter);
+  if (filterImage.length === 0) {
+    console.log(pictures);
+    sendEmptyBlock('no-filters', divContainer);
+  } else{
+    divContainer.innerHTML = '';
+  }
   renderPictures(filterImage);
 
   var activeFilter = formFilters.querySelector('.' + ACTIVE_FILTER_CLASSNAME);
@@ -84,35 +91,58 @@ var SetFiltrationImgId = function(filter) {
   var filterToActivate = document.getElementById(filter);
   filterToActivate.classList.add(ACTIVE_FILTER_CLASSNAME);
 };
-/**
- * @param {Array.<Object>} hotels
- * @param {string} filter
- */
- var getFilterPictures = function(pictures, filter) {
+// /**
+//  * @param {Array.<Object>} hotels
+//  * @param {string} filter
+//  */
+var getFilterPictures = function(pictures, filter) {
   var picturesToFilter = pictures.slice(0);
 
   switch (filter) {
     case 'filter-popular':
       return picturesToFilter;
-    break;
+      break;
     case 'filter-new':
-      picturesToFilter.sort(function(a, b) {
-        return a.price - b.price;
+      var newFilterImg = picturesToFilter.filter(function(item) {
+        var newDate = new Date();
+        var imgDateArray = item.date.split('-');
+        var imgDate = new Date(imgDateArray);
+        if ((newDate - imgDate) <= 4 * 24 * 60 * 60 * 1000) {
+          return item;
+        }
       });
-    break;
+      newFilterImg.sort(function(a, b) {
+        a = new Date(a.date.split('-'));
+        b = new Date(b.date.split('-'));
+        return b - a;
+      });
+      return newFilterImg;
+      break;
     case 'filter-discussed':
       picturesToFilter.sort(function(a, b) {
-        return a.price - b.price;
+        return a.comments - b.comments;
       });
+      break;
   }
   return picturesToFilter;
 };
-// Функция добавит обработчики клика элементам фильтра
-var SetFiltrationImg = function() {
-  var filtersName = formFilters.querySelectorAll('.filters-item');
+// // Функция добавит обработчики клика элементам фильтра
+var setFiltrationImg = function() {
+  var filtersName = formFilters.querySelectorAll('.filters-radio');
   for (var i = 0; i < filtersName.length; i++) {
-    filtersName[i].onclick = functon(evt) {
-      SetFiltrationImgId(this.id);
-    }
+    filtersName[i].onclick = function() {
+      setFiltrationImgId(this.id);
+    };
   }
+};
+// /**
+//  * @param {string>} class
+//  * @param {string} container
+//  */
+var sendEmptyBlock = function(filterclass, container) {
+  var div = document.createElement('div');
+  div.classList.add(filterclass);
+  div.textContent = 'ERROR';
+  container.appendChild(div);
+  console.log(div);
 };
